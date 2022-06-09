9638271051a04a2e1daf112088484710a4c70ef1
Kyongnam Kang <kangkna@gmail.com>
2022-06-09 16:50:51 -0400

    Work with strings

--- methods_variables.rb
+++ methods_variables.rb
@@ -7,7 +7,11 @@
 #  call downcase on the variable, print it out
 #  call reverse on the variable, print it out
 #  call length on the variable, print it out
-
+string="Why so serious?"
+puts string.upcase
+puts string.downcase
+puts string.reverse
+puts string.length
 
 
 
@@ -20,13 +24,9 @@
 user_name = "coco_11am"
 last_login = "12/09/2021"
 
-# 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do. 
+# 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do.
   # Write the methods out on the lines below, with an explanation in your own words of how they work.
 
 
 
 # 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does, when at the end of a method name. Show your understading by providing an example and writing an explanation.
-
-
-
-
