161d815467785b30c31581712a1ca828c0836626
Kyongnam Kang <kangkna@gmail.com>
2022-06-09 17:37:59 -0400

    Finish part 2

--- methods_variables.rb
+++ methods_variables.rb
@@ -26,7 +26,18 @@ last_login = "12/09/2021"
 
 # 1: In the Ruby Docs for String methods (https://ruby-doc.org/core-3.1.0/String.html), find 4 methods you have not yet used and call them on one of the variables above. Between reading the documentation and reading the output from calling the methods, make sense of what they do.
   # Write the methods out on the lines below, with an explanation in your own words of how they work.
-
-
-
+#This function will insert whichever index order with the substring
+#Output will be "coceo_11am"
+user_name.insert (3,'e')
+#This function will replace main string with the substring
+#Output will be "coco_10am"
+user_name.replace ("coco_10am")
+#This function will delete what is on the main string
+#Output will be "coco"
+user_name.delete! ("_11am")
+#This function will clear out the main strings
+#Output will be nothing because the main string has been cleared
+user_name.clear
 # 2: Do some research (either testing out with your own code or Googling) to build an understanding of what the `!` does, when at the end of a method name. Show your understading by providing an example and writing an explanation.
+# Using ! gives back the modified version of the origin
+# e.g "Hello".downcase will give out the output of "hello" giving back the origin however, "Hello".downcase! will give out the modified version
