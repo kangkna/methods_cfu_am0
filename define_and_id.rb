57c7fb49a27cd4b63491636e00a5fa03aa5f6314
Kyongnam Kang <kangkna@gmail.com>
2022-06-09 16:42:18 -0400

    Initial commit

--- define_and_id.rb
+++ define_and_id.rb
@@ -8,20 +8,23 @@
 "Hello World".downcase
 
 # YOU DO the rest:
+#include? is checking whether or not the string "Hello World" in fact does include "Hello"
+#The return value is true
 
+puts "Hello World".include?("Hello")
 
-"Hello World".include?("Hello")
+#end_with? is checking whether or not the string "Hello World" does in fact end with string "Hello"
+#The return value is false
+puts "Hello World".end_with?("Hello")
 
+#end_with? is checking whether or not the string "Hello World" does in fact end withe string "rld"
+#The return value is false
+puts "Hello World".end_with?("rld")
 
-"Hello World".end_with?("Hello")
+#even? is checking whether or not the integer 12 is in fact even or not
+#The return value is false
+puts 12.even?
 
-
-"Hello World".end_with?("rld")
-
-
-12.even?
-
-
-18.next
-
-
+#next function enables the number that comes after
+#The return value is 19
+puts 18.next
